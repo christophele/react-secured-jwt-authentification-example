d8408ead4b5129ddea091f2bafd93a7299cbbfb8
cle <le_christophe@hotmail.fr>
2018-11-27 18:27:40 +0100

    es6 syntax

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -11,26 +11,26 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3002';
 
 // action creator
-export function setAuthentification(isLoggedIn) {
+export const setAuthentification = isLoggedIn => {
     return {
         type: SET_AUTHENTIFICATION,
         payload: isLoggedIn
     };
 }
 
-export function incrementActionCount() {
+export const incrementActionCount = () => {
     return {
         type: INCREMENT_ACTION_COUNT
     };
 }
 
-export function addRessources() {
+export const addRessources = () => {
     return {
         type: ADD_RESSOURCES
     }
 }
 
-export function signinUser({email, password}, history) {
+export const signinUser = ({email, password}, history) => {
     return function(dispatch) {
         axios.post(`${BASE_URL}/signin`, {
             email,
@@ -46,14 +46,14 @@ export function signinUser({email, password}, history) {
     };
 }
 
-export function signoutUser() {
+export const signoutUser = () => {
     return function(dispatch) {
         dispatch(setAuthentification(false))
         localStorage.removeItem("token");
     }
 }
 
-export function signupUser({email, password}, history) {
+export const signupUser = ({email, password}, history) => {
     return function(dispatch) {
         axios.post(`${BASE_URL}/signup`, {
             email,
@@ -69,7 +69,7 @@ export function signupUser({email, password}, history) {
     };
 }
 
-export function getSecretRessource() {
+export const getSecretRessource = () => {
     return function(dispatch) {
         axios
         .get(`${BASE_URL}/secretRessource`, {
@@ -88,14 +88,14 @@ export function getSecretRessource() {
     }
 }
 
-export function parseError(errorMessage) {
+export const parseError = errorMessage => {
     return {
         type: PARSE_ERROR,
         payload: errorMessage
     }
 }
 
-export function resetError() {
+export const resetError = () => {
     return {
         type: RESET_ERROR
     }
